17c7148584fae38d39519dcc7473b29b72093447
Karina <amapolanueva96@gmail.com>
2025-10-04 00:41:49 -0700

    add client form works correctly

--- src/frontend/js/add_new_client_form.js
+++ src/frontend/js/add_new_client_form.js
@@ -1,16 +1,39 @@
-document.addEventListener("DOMContentLoaded", () => {
+async function formSubmit(event) {
+    event.preventDefault();
 
-    const form = document.getElementById("add_client_form")
-
-
-    form.addEventListener("submit",() => {
-        //send the request to update the client
-        fetch(`http://127.0.0.1:5000//api/v1/clients`,{
+    const clientData = {
+        id: event.target["id"].value,
+        name: event.target["name"].value,
+        type: event.target["type"].value,
+        phone_number: event.target["phone_number"].value,
+        address_line1: event.target["address_line1"].value,
+        address_line2: event.target["address_line2"].value,
+        address_line3: event.target["address_line3"].value,
+        city: event.target["city"].value,
+        state: event.target["state"].value,
+        zip_code: event.target["zip_code"].value,
+        country: event.target["country"].value
+    }
+    //send the request to update the client
+    try {
+        const res = await fetch(`http://127.0.0.1:5000/api/v1/clients`, {
             method: "POST",
-            headers: {"Content-type": "application/json"}
-        })
-        
+            headers: { "Content-type": "application/json" },
+            body: JSON.stringify(clientData)
+        });
+
+        const data = await res.json();
+
+        console.log("Server response:", data);
         alert("Submitted");
-    });
-    
+
+    } catch (err) {
+        console.log("Error:", err);
+    }
+
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    const form = document.getElementById("add_new_client_form")
+    form.addEventListener("submit", formSubmit);
 });
--- src/frontend/js/update_client_form.js
+++ src/frontend/js/update_client_form.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     form.addEventListener("submit",() => {
         //send the request to update the client
-        fetch(`http://127.0.0.1:5000//api/v1/clients/${document.getElementById}`,{
+        fetch(`http://127.0.0.1:5000/api/v1/clients/${document.getElementById}`,{
             method: "PUT",
             headers: {"Content-type": "application/json"}
         })
