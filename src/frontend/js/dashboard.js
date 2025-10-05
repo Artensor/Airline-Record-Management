b9c581995546bbe465a304d97843b60e4f543797
Karina <amapolanueva96@gmail.com>
2025-10-04 18:19:36 -0700

    build out basic tabs structure in dashboard html file

--- src/frontend/js/dashboard.js
+++ src/frontend/js/dashboard.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded",() => {
+  const headingsClients = ["ID", "Type", ];
+  const headingsAirlines = [];
+  const headingsFlights = [];
+
+  const tabs = document.querySelectorAll(".tab")
+
+  tabs.forEach(element => {
     
+  
+    if(headingsClients){
+
+    }else if(headingsAirlines){
+
+    }else{
+    
+    }
+
+  });
 })
--- src/frontend/js/update_client_form.js
+++ src/frontend/js/update_client_form.js
@@ -1,12 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-    const form = document.getElementById("edit_form")
-
-    
+    const form = document.getElementById("update_client_form")
 
     form.addEventListener("submit",() => {
+        
+        const id = document.getElementById("id").value;
+
         //send the request to update the client
-        fetch(`http://127.0.0.1:5000/api/v1/clients/${document.getElementById}`,{
+        fetch(`http://127.0.0.1:5000/api/v1/clients/${id}`,{
             method: "PUT",
             headers: {"Content-type": "application/json"}
         })
