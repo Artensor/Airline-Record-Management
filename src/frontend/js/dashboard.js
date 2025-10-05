05aa3dca87274f6ead9472a8ecebd075dd0096d3
Karina <amapolanueva96@gmail.com>
2025-10-05 13:30:43 -0700

    reorganizes the dashboard JS file

--- src/frontend/js/dashboard.js
+++ src/frontend/js/dashboard.js
@@ -1,82 +1,67 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const headings = {
-    clients: [{ label: "ID", key: "id" }, { label: "Type", key: "type" }, { label: "Name", key: "name" }, { label: "Address Line 1", key: "adress_line2" }, { label: "Address Line 2", key: "adress_line3" }, { label: "Address Line 3", key: "adress_line1" }, { label: "City", key: "city" }, { label: "State", key: "state" }, { label: "Country", key: "country" }, { label: "Phone", key: "phone_numnber" }, { label: "Actions" }],
-    flights: [{ label: "Client", key: "client_id" }, { label: "Airline", key: "airline_id" }, { label: "date", key: "date" }, { label: "Start City", key: "start_city" }, { label: "End City", key: "end_city" }, { label: "Acitions" }],
-    airlines: [{ label: "Company Name", key: "company_name" }, { label: "Actions", key: "actions" }]
-  }
 
 
-  const tabs = document.querySelectorAll(".tab")
+const headings = {
+  clients: [{ label: "ID", key: "id" }, { label: "Type", key: "type" }, { label: "Name", key: "name" }, { label: "Address Line 1", key: "adress_line2" }, { label: "Address Line 2", key: "adress_line3" }, { label: "Address Line 3", key: "adress_line1" }, { label: "City", key: "city" }, { label: "State", key: "state" }, { label: "Country", key: "country" }, { label: "Phone", key: "phone_numnber" }, { label: "Actions" }],
+  flights: [{ label: "Client", key: "client_id" }, { label: "Airline", key: "airline_id" }, { label: "date", key: "date" }, { label: "Start City", key: "start_city" }, { label: "End City", key: "end_city" }, { label: "Acitions" }],
+  airlines: [{ label: "Company Name", key: "company_name" }, { label: "Actions", key: "actions" }]
+}
 
-  const contents = document.querySelectorAll(".tab-content")
+const endpoints = {
+  clients: "/api/v1/clients",
+  flights: "/api/v1/flights",
+  airlines: "/api/v1/airlines"
 
-  //removes active class from all tab buttons and adds hidden class to their content
-  function resetTabs() {
-    tabs.forEach(tab => tab.classList.remove("active"));
-    contents.forEach(content => content.classList.add("hidden"));
-  }
+}
 
-  tabs.forEach(tab => {
-    tab.addEventListener("click", async () => {
 
-      const type = tab.dataset.type;
-      const endpoint = tab.dataset.endpoint;
-      //get the div container content of the selected tab with the id
-      const selectedTabContent = document.getElementById(tab.dataset.tab)
+//removes active class from all tab buttons and adds hidden class to their content
+function resetTabs(tabs, contents) {
+  tabs.forEach(tab => tab.classList.remove("active"));
+  contents.forEach(content => content.setAttribute("hidden", ""));
+}
 
-      resetTabs();
 
-      //updates active tab
-      tab.classList.add("active");
-      selectedTabContent.classList.remove("hidden");
-
-      //update url with selected tab params
-      const url = new URL(window.location.href);
-      url.searchParams.set("tab", type);
-      window.history.pushState(null, '', url.toString());
-
-      //fetch data for tab data table
-      try {
-        const res = await fetch(`http://127.0.0.1:5000${endpoint}`);
-
-        const data = await res.json();
-
-        const tableHeadings = headings[type];
-
-        selectedTabContent.innerHTML = table(data, tableHeadings, type);
-
-        console.log("Server response:", data);
-
-      } catch (err) {
-        console.log("Error:", err);
-      }
-
-    })
-
-    async function deleteRecord(type, id) {
-
+//fetch data for tab data table
+async function fetchTableData(endpoint) {
+  try {
+    const res = await fetch(`http://127.0.0.1:5000${endpoint}`);
+    if (!res.ok) {
+      console.error(`Fetch failed with status ${res.status}`);
+      return { data: [] };
     }
 
-    //builds the table for each tab record selected
-    function table(data, tableHeadings, type) {
-      const headerRow = tableHeadings.map(h => `<th>${h.label}</th>`).join("");
-      const bodyRows = data.data.map(item =>
-        `<tr>
+    const json = await res.json();
+    console.log("Response:", json);
+    return json;
+  } catch (err) {
+    console.error("Eerror:", err);
+    return { data: [] };
+  }
+}
+
+async function deleteRecord(type, id) {
+
+}
+
+//builds the table for each tab record selected
+function table(data, type, tableHeadings) {
+  const headerRow = tableHeadings.map(h => `<th>${h.label}</th>`).join("");
+  const bodyRows = data.data.map(item =>
+    `<tr>
     ${tableHeadings.map(h => {
-          if (!h.key) {
-            return `<td><a href="update_${type}_form.html?id=${item.id}">Edit</a>
+      if (!h.key) {
+        return `<td><a href="update_${type}_form.html?id=${item.id}">Edit</a>
         <button onclick="deleteRecord('${type}', '${item.id}')">Delete</button></td>`
-          }
-          else {
-            return `<td>${item[h.key] ?? ""}</td>`
+      }
+      else {
+        return `<td>${item[h.key] ?? ""}</td>`
 
-          }
-        }).join("")}
-    
+      }
+    }).join("")}
     </tr>`
-      ).join("");
+  ).join("");
 
-    return `
+  return `
     <div class="table-wrapper">
     <div class="table-header">
     
@@ -88,6 +73,28 @@ document.addEventListener("DOMContentLoaded", () => {
     </table>
     </div>
   `;
-    }
-  });
+}
+
+
+document.addEventListener("DOMContentLoaded", async () => {
+
+  const content = document.getElementById("content")
+  const type = new URLSearchParams(window.location.search).get("tab") || "clients";
+  const tableHeadings = headings[type];
+
+  const endpoint = endpoints[type];
+  const data = await fetchTableData(endpoint);
+  const html = table(data, type, tableHeadings);
+  content.innerHTML = html;
+
+  const tabs = document.querySelectorAll(".tab");
+  const contents = document.querySelectorAll(".content");
+ 
+  resetTabs(tabs, contents);
+
+  //updates active tab
+  tabs.forEach(tab => { tab.classList.add("active"); })
+  tabs.forEach(tab => { tab.classList.add("active"); })
+  content.removeAttribute("hidden");
+
 })
--- src/frontend/js/update_clients_form.js
+++ src/frontend/js/update_clients_form.js
@@ -1,6 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
 
     const form = document.getElementById("update_client_form")
+    
+    //get record params from the url
+    const params = new URLSearchParams(window.location.search);
+    const id = params.get("id");
+
+
+    //get all the filled data
+    if(id){
+        fetch(`http://127.0.0.1:5000/api/v1/clients/${id}`)
+        .then(res => res.json)
+        .then( client => {
+            form.elements["name"].value = client.name || "";
+            form.elements["type"].value = client.type || "";
+            form.elements["phone_number"].value = client.phone_number || ""; 
+        })
+
+    }
 
     form.addEventListener("submit",() => {
         
