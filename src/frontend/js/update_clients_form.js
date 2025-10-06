0ba887561c2ff01f61ca01bb0d6c881a0696170a
Karina <amapolanueva96@gmail.com>
2025-10-05 17:55:12 -0700

    finished update_clients_form

--- src/frontend/js/update_clients_form.js
+++ src/frontend/js/update_clients_form.js
@@ -1,35 +1,64 @@
 document.addEventListener("DOMContentLoaded", () => {
 
+    //get form from the page
     const form = document.getElementById("update_client_form")
-    
-    //get record params from the url
+
+    // get record params from the url
     const params = new URLSearchParams(window.location.search);
     const id = params.get("id");
+    getRecordData(id);
 
 
-    //get all the filled data
-    if(id){
-        fetch(`http://127.0.0.1:5000/api/v1/clients/${id}`)
-        .then(res => res.json)
-        .then( client => {
-            form.elements["name"].value = client.name || "";
-            form.elements["type"].value = client.type || "";
-            form.elements["phone_number"].value = client.phone_number || ""; 
-        })
+    //get all data and fill the update form
+    async function getRecordData(id) {
+
+        if (id) {
+
+            fetch(`http://127.0.0.1:5000/api/v1/clients/${id}`)
+                .then(res => res.json())
+                .then(client => {
+                    form.elements["name"].value = client.name || "";
+                    form.elements["type"].value = client.type || "";
+                    form.elements["phone_number"].value = client.phone_number || "";
+                    form.elements["address_line1"].value = client.address_line1 || "";
+                    form.elements["address_line2"].value = client.address_line2 || "";
+                    form.elements["address_line3"].value = client.address_line3 || "";
+                    form.elements["city"].value = client.city || "";
+                    form.elements["state"].value = client.state || "";
+                    form.elements["zip_code"].value = client.zip_code || "";
+                    form.elements["country"].value = client.country || "";
+                })
+
+        }
 
     }
 
-    form.addEventListener("submit",() => {
-        
-        const id = document.getElementById("id").value;
+    form.addEventListener("submit", async (event) => {
+        event.preventDefault();
+
+        const updatedClient = {
+            id,
+            name: form.elements["name"].value,
+            type: form.elements["type"].value,
+            phone_number: form.elements["phone_number"].value,
+            address_line1: form.elements["address_line1"].value,
+            address_line2: form.elements["address_line2"].value,
+            address_line3: form.elements["address_line3"].value,
+            city: form.elements["city"].value,
+            state: form.elements["state"].value,
+            zip_code: form.elements["zip_code"].value,
+            country: form.elements["country"].value
+        };
+
 
         //send the request to update the client
-        fetch(`http://127.0.0.1:5000/api/v1/clients/${id}`,{
+        fetch(`http://127.0.0.1:5000/api/v1/clients/${id}`, {
             method: "PUT",
-            headers: {"Content-type": "application/json"}
+            headers: { "Content-type": "application/json" },
+            body: JSON.stringify(updatedClient)
         })
-        
+
         alert("Submitted");
     });
-    
+
 });
