b05781c07250b84873b67b00bd62e03468fe21dc
Karina <amapolanueva96@gmail.com>
2025-10-10 13:45:50 -0700

    refactors client update form JS to make it a module for tests

--- src/frontend/js/update_airlines_form.js
+++ src/frontend/js/update_airlines_form.js
@@ -1,3 +1,42 @@
+
+//get all data and fill the update form
+async function getRecordData(id, form) {
+
+    if (id) {
+
+        fetch(`http://127.0.0.1:5000/api/v1/airlines/${id}`)
+            .then(res => res.json())
+            .then(airline => {
+                form.elements["company_name"].value = airline.company_name || "";
+                form.elements["type"].value = airline.type || "";
+            })
+
+    }
+
+}
+
+const formSubmit = (id) => async (event) =>  {
+    event.preventDefault();
+    const form = event.target;
+
+    //updates inputs
+    const updatedAirline = {
+        company_name: form.elements["company_name"].value,
+        type: form.elements["type"].value,
+    };
+
+
+    //send the request to update the airline
+    fetch(`http://127.0.0.1:5000/api/v1/airlines/${id}`, {
+        method: "PUT",
+        headers: { "Content-type": "application/json" },
+        body: JSON.stringify(updatedAirline)
+    })
+    window.location.href = "dashboard.html?tab=airlines";
+    alert("Submitted");
+};
+
+
 document.addEventListener("DOMContentLoaded", () => {
 
     //get form from the page
@@ -6,43 +45,6 @@ document.addEventListener("DOMContentLoaded", () => {
     // get record params from the url
     const params = new URLSearchParams(window.location.search);
     const id = params.get("id");
-    getRecordData(id);
-
-
-    //get all data and fill the update form
-    async function getRecordData(id) {
-
-        if (id) {
-
-            fetch(`http://127.0.0.1:5000/api/v1/airlines/${id}`)
-                .then(res => res.json())
-                .then(airline => {
-                    form.elements["company_name"].value = airline.company_name || "";
-                    form.elements["type"].value = airline.type || "";
-                })
-
-        }
-
-    }
-
-    form.addEventListener("submit", async (event) => {
-        event.preventDefault();
-        
-        //updates inputs
-        const updatedAirline = {
-            company_name: form.elements["company_name"].value,
-            type: form.elements["type"].value,
-        };
-
-
-        //send the request to update the airline
-        fetch(`http://127.0.0.1:5000/api/v1/airlines/${id}`, {
-            method: "PUT",
-            headers: { "Content-type": "application/json" },
-            body: JSON.stringify(updatedAirline)
-        })
-        window.location.href = "dashboard.html?tab=airlines";
-        alert("Submitted");
-    });
-
+    getRecordData(id, form);
+    form.addEventListener("submit", formSubmit(id));
 });
