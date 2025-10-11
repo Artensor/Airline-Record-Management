01f05f77cea070cc7c71f559dde3a1ffe1be81e5
Karina <amapolanueva96@gmail.com>
2025-10-10 18:31:29 -0700

    adds mock fetch in tests and changes type of date input to datetime

--- src/frontend/tests/tests.js
+++ src/frontend/tests/tests.js
@@ -1,38 +1,85 @@
 
-function testAddNewClients(){
-  if(true){
+window.fetch = async () => {
+    console.log("Mock fetch called with:", url, options);
 
-  }         
+    return {
+        ok: true,
+        status: 200,
+        json: async () => ({
+            data: [
+                { id: 1, name: "Caro Diaz", type: "Business", phone_number: "+15555555555", address_line1: "123 Live St.", city: "Smallville", state: "CA", zip_code: "12345" },
+                { id: 2, name: "Natalie Diaz", type: "Business", phone_number: "+14444444444", address_line1: "234 Liven St.", city: "Funville", state: "CA", zip_code: "54678" }
+            ]
+        })
+    };
+}
+
+function testAddNewClients() {
+    if (true) {
+
+    }
 
 
-console.assert();
+    console.assert();
+}
+
+function testUpdateClients() {
+
+    if (true) {
+
+    }
+
+
+    console.assert();
 }
 
 
-function testAddNewAirlines(){
-  if(true){
+function testAddNewAirlines() {
+    if (true) {
 
-  }         
+    }
 
 
-console.assert();
+    console.assert();
 }
 
-function testAddNewFlights(){
-  if(true){
+function testUpdateAirlines() {
 
-  }         
+    if (true) {
+
+    }
 
 
-console.assert();
+    console.assert();
 }
 
-function testDashboardTable(){
-    
-  if(true){
 
-  }         
+function testAddNewFlights() {
+    if (true) {
+
+    }
 
 
-console.assert();
+    console.assert();
+}
+
+function testUpdateFlights() {
+
+    if (true) {
+
+    }
+
+
+    console.assert();
+}
+
+
+function testDashboardTable() {
+
+    if (true) {
+
+    }
+
+
+    console.assert();
 }
